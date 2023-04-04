c1c8a6f521ce3aafc1a8af4b715d25ef329e23ef
v.dunkan <v.dunkan@gmail.com>
2023-04-04 22:46:20 +0300

    by Skiff

--- classes.py
+++ classes.py
@@ -14,30 +14,39 @@ class Phone(Field):
 class Record:
     def __init__(self, name: Name, phone: Phone = None):
         self.name = name
-        if phone is not None:
-            self.phone = [phone]
-        else:
-            self.phone = []
+        self.phones = []
+        # if phone is not None:
+        #     self.phone = [phone]
+        # else:
+        if phone:
+            self.add_phone_field(phone)
 
     def add_phone_field(self, phone_number: Phone):
-        self.phone.append(phone_number)
+        self.phones.append(phone_number)
 
     def change_phone_field(self, old_number: Phone, new_number: Phone):
-        try: 
-            self.phone.remove(old_number)
-            self.phone.append(new_number)
-        except ValueError:
-            return f"Contact does not contain such phone number: {old_number}"
+        for i, p in enumerate(self.phones):
+            if p.value == old_number.value:
+                self.phones[i] = new_number
+                return f"Phone {old_number.value} changed on {new_number.value}"
+        return f"Contact does not contain such phone number: {old_number}"
+        # try: 
+        #     self.phones.remove(old_number)
+        #     self.phones.append(new_number)
+        # except ValueError:
+        #     return f"Contact does not contain such phone number: {old_number}"
         
     def delete_phone_field(self, phone: Phone):
         try:
-            self.phone.remove(phone)
+            self.phones.remove(phone)
         except ValueError:
             return f"Contact does not contain such phone number: {phone}"
         
 class AddressBook(UserDict):
 
-    def add_record(self, name : Name, phone: Phone = None):
-        new_contact = Record(name=name, phone=phone)
-        self.data[name.value] = new_contact
-        
+    def add_record(self, rec: Record):
+        # new_contact = Record(name=name, phone=phone)
+        self.data[rec.name.value] = rec
+    
+    def show_all(self):
+        return '\n'.join([f'{r.name.value : r.phones}' for r in self.data])
